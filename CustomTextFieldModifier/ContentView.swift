84f67c4e68ca0ede9a1f8e57e5df6b2d45527089
devkilyungi <victorkilyungi@gmail.com>
2024-04-05 13:39:40 +0300

    Initial Commit

---
+++ CustomTextFieldModifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomTextFieldModifier
+//
+//  Created by Victor Kilyungi on 05/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomTextFieldModifier/CustomTextFieldModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldModifierApp.swift
+//  CustomTextFieldModifier
+//
+//  Created by Victor Kilyungi on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
